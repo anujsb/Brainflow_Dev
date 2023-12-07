6b5dac1e2ab39039ca43deb75513f47b6449dcbb
anujsb <anujsbhuyar@gmail.com>
2023-12-07 22:25:48 +0530

    edited service cards
    col-12

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -4,18 +4,21 @@ const Services = () => {
   return (
     <div className="background-hero ">
       <div className="flex mr-10 ml-10">
-        <div className="card card-25 shadow-xl">1</div>
-        <div className="card card-50 shadow-xl">2</div>
-        <div className="card card-25 shadow-xl">3</div>
+        <div className="card col-8 shadow-xl">
+          
+            <h3 className="col-12">ggg</h3>
+          
+        </div>
+        <div className="card col-8 shadow-xl">2</div>
+        <div className="card col-8 shadow-xl">3</div>
       </div>
 
       <div className="flex mr-10 ml-10">
-        <div className="card card-50 shadow-xl">4</div>
-        <div className="card card-50 shadow-xl">5</div>
-        {/* <div className="card card-25 shadow-xl">3</div> */}
+        <div className="card col-12 shadow-xl">4</div>
+        <div className="card col-12 shadow-xl">5</div>
       </div>
     </div>
-  );
+  );  
 };
 
 export default Services;
